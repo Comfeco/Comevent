be7e97043d38e0cc26bc3f7eeb1a9b68bcc8ec51
David Montoya <deividelpaisa@hotmail.com>
2023-10-30 20:56:03 -0500

    style: [C-51] 🎨 navbar layout

--- frontend/app/src/common/layouts/core-layout/components/organisms/nav/nav.component.ts
+++ frontend/app/src/common/layouts/core-layout/components/organisms/nav/nav.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IconComeventComponent, TitleComponent } from '@ui/components';
+import {
+  ButtonComponent,
+  IconComeventComponent,
+  TitleComponent,
+} from '@ui/components';
 
 @Component({
   standalone: true,
   selector: 'c-nav',
-  imports: [CommonModule, IconComeventComponent, TitleComponent, RouterModule],
+  imports: [
+    CommonModule,
+    IconComeventComponent,
+    TitleComponent,
+    RouterModule,
+    ButtonComponent,
+  ],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
