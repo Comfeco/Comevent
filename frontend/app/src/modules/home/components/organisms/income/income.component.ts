4b88008ce143062f792a23564c7cee847d6bb122
David Montoya <deividelpaisa@hotmail.com>
2023-11-24 17:38:15 -0500

    style: [C-58] 🎨 The profit section of the home page is translated into 2 languages

--- frontend/app/src/modules/home/components/organisms/income/income.component.ts
+++ frontend/app/src/modules/home/components/organisms/income/income.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { ButtonComponent, TitleComponent } from '@ui/components';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, TitleComponent, ButtonComponent],
+  imports: [CommonModule, TitleComponent, ButtonComponent, TranslateModule],
   selector: 'income',
   templateUrl: './income.component.html',
   styleUrls: ['./income.component.scss'],
